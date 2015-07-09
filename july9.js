fc33f719355c48bf09e79c3a98bfb5000870f5e2
ChipMorris10 <goldcomac@mac.com>
2015-07-09 15:20:06 -0600

    July 9 Assignment

---
+++ july9.js
@@ -0,0 +1,16 @@
+// This produces a very simple prompt //
+// and returns by saying Hello to the user //
+
+// var name stores the variable in the browser
+// I can confirm this by opening up the console
+// and typing in name
+
+// prompt puts out a dialog box asking the user
+// to enter their name
+
+var name = prompt("What's your name?");
+
+// alert retuns another dialog box with the information
+// in the parenthesis
+
+alert("Hello " +name +"!")
